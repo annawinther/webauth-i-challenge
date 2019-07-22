4b0a0f0d598b9dd6dcc90fc547e6b1085984934d
Anna W <anski.anna@gmail.com>
2019-07-22 14:16:48 +0200

    added post to the register endpoint to register new user. password stored as hash

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
-const Users = require('./users/users-model.js');
+const Users = require('./users/userDb')
 
 const server = express();
 
@@ -15,7 +15,18 @@ server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
-
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12)
+    
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+})
 
 const port = process.env.PORT || 3003;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
