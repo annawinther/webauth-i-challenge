49f399de6f333b2c7515c4298637ed8a8cdf506a
Anna W <anski.anna@gmail.com>
2019-07-22 13:06:27 +0200

    got project up

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users/users-model.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+
+
+const port = process.env.PORT || 3003;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
