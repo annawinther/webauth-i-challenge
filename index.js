099901ad0b00c15ef0c23e067e6adfa79722d764
Anna W <anski.anna@gmail.com>
2019-07-23 10:17:27 +0200

    fixed bug with login endpoint, now works

--- index.js
+++ index.js
@@ -28,5 +28,21 @@ server.post('/api/register', (req, res) => {
         })
 })
 
+server.post('/api/login', (req, res) => {
+
+    let { username, password } = req.body;
+    Users.findBy({ username })
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user[0].password)){
+                res.status(200).json(`welcome ${username}!`)
+            } else {
+                res.status(400).json('could not find user')
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: "could not log in"})
+        })
+})
+
 const port = process.env.PORT || 3003;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
