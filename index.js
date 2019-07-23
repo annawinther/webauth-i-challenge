8e16e68c8cfb7a661edc67a75971cd01b87abaa0
Anna W <anski.anna@gmail.com>
2019-07-23 13:16:52 +0200

    started to add register and log in and store it using cookies and sessions

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
+// const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const Users = require('./users/userDb')
 
 const server = express();
@@ -11,13 +14,32 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use(session({
+    name: 'sessionId',
+    secret: 'keep it secret, keep it short',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+
+}))
+
 server.get('/', (req, res) => {
   res.send("It's alive!");
 });
 
+
+
+
 server.post('/api/register', (req, res) => {
     let user = req.body;
-    user.password = bcrypt.hashSync(user.password, 12)
+    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    user.password = hash;
+    // let user = req.body;
+    // user.password = bcrypt.hashSync(user.password, 12)
     
     Users.add(user)
         .then(saved => {
@@ -29,18 +51,19 @@ server.post('/api/register', (req, res) => {
 })
 
 server.post('/api/login', (req, res) => {
-
     let { username, password } = req.body;
+
     Users.findBy({ username })
         .then(user => {
             if(user && bcrypt.compareSync(password, user[0].password)){
+                req.session.user = user;
                 res.status(200).json(`welcome ${username}!`)
             } else {
                 res.status(400).json('could not find user')
             }
         })
         .catch(error => {
-            res.status(500).json({message: "could not log in"})
+            res.status(500).json({message: "could not log in" })
         })
 })
 
